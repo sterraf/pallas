9035eb46c1c6d27dfa54795ca6e0fbc56a85e0c6
Franco Luque <francolq@gmail.com>
2024-09-04 16:51:49 -0300

    port pallas-codec to no_std

--- pallas-codec/src/flat/decode/decoder.rs
+++ pallas-codec/src/flat/decode/decoder.rs
@@ -1,6 +1,8 @@
 use super::Decode;
 use super::Error;
 use crate::flat::zigzag::ZigZag;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 #[cfg(feature = "num-bigint")]
 use num_bigint::{BigInt, BigUint};
@@ -104,13 +106,13 @@ impl<'b> Decoder<'b> {
     }
 
     // TODO: Do we need this?
-    pub fn string(&mut self) -> Result<String, Error> {
-        let mut s = String::new();
-        while self.bit()? {
-            s += &self.char()?.to_string();
-        }
-        Ok(s)
-    }
+    // pub fn string(&mut self) -> Result<String, Error> {
+    //     let mut s = String::new();
+    //     while self.bit()? {
+    //         s += &self.char()?.to_string();
+    //     }
+    //     Ok(s)
+    // }
 
     /// Decode a string.
     /// Convert to byte array and then use byte array decoding.
